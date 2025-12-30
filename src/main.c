a189b5d0db32a69f792635e591d93d60d80fb3e0
shiroime <hela.koz@interia.pl>
2025-12-30 20:04:45 +0100

    Dodanie pola masa do struktury Dinozaur

--- src/main.c
+++ src/main.c
@@ -9,6 +9,7 @@ typedef enum {
 
 typedef struct {
     Dieta dieta;
+    double masa;
 } Dinozaur;
 
 int main (void) 
@@ -16,8 +17,10 @@ int main (void)
     Dinozaur d1;
 
     d1.dieta = MIEZOZERNY;
+    d1.masa = 1;
 
     printf("Dinozaur ma diete: %d\n", d1.dieta);
+    printf("Dinozaur ma mase: %2.f\n", d1.masa);
 
     return 0;
 }
