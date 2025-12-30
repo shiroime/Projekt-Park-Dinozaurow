a0d0f02e6325c49b68300a55fe5d029cae539055
shiroime <hela.koz@interia.pl>
2025-12-30 19:56:56 +0100

    Dodanie podstawowego enum oraz struktury Dinozaur

--- src/main.c
+++ src/main.c
@@ -1,5 +1,23 @@
-#include 
-int main (void) {
-    printf("System Zarzadzania Parkiem Dinozaurow\n");
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef enum {
+    MIEZOZERNY,
+    ROSLINOZERNY,
+    WSZYSTKOZERNY
+} Dieta;
+
+typedef struct {
+    Dieta dieta;
+} Dinozaur;
+
+int main (void) 
+{
+    Dinozaur d1;
+
+    d1.dieta = MIEZOZERNY;
+
+    printf("Dinozaur ma diete: %d\n", d1.dieta);
+
     return 0;
 }
